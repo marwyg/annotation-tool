marwyg/annotation-tool

commit b154c7b8a6767303fd9e9a443373d3c8f13faa33

Update dependencies

frontend/Gruntfile.js
@@ -83,7 +83,7 @@ module.exports = function (grunt) {
                 maxWarnings: grunt.option('maxWarnings'),
                 fix: grunt.option('fix')
             },
-            files: ['js/**/*.js', './{,.}*.js', '!js/libs/**/{,.}*']
+            files: ['.']
         },
 
         stylelint: {

frontend/.eslintrc.js
@@ -1,52 +1,0 @@
-module.exports = {
-    ignorePatterns: [
-        '/js/libs',
-        'src',
-        'target',
-        '!.*'
-    ],
-    rules: {
-        'no-trailing-spaces': 'warn',
-        'keyword-spacing': 'warn',
-        'space-before-function-paren': ['warn', {
-            named: 'never',
-            anonymous: 'always',
-            asyncArrow: 'always'
-        }],
-        'function-call-argument-newline': ['warn', 'consistent'],
-        'block-spacing': 'warn',
-        'object-curly-spacing': ['warn', 'always'],
-        'key-spacing': 'warn',
-        'comma-dangle': 'warn',
-        'quotes': 'warn',
-        'no-multiple-empty-lines': ['warn', { max: 1 }],
-        'semi': 'warn',
-        'indent': ['warn', 4, {
-            ignoredNodes: [
-                'Program > ExpressionStatement > CallExpression[callee.name="define"] > FunctionExpression.arguments > .body'
-            ],
-            VariableDeclarator: 0
-        }],
-        'one-var': ['warn', 'never'],
-        'space-infix-ops': 'warn',
-        'no-multi-spaces': 'warn',
-        'space-before-blocks': 'warn'
-    },
-    overrides: [{
-        files: './{,.}*.js',
-        env: {
-            node: true,
-            es2020: true
-        },
-        rules: {
-            'quotes': ['warn', 'single']
-        }
-    }, {
-        files: 'js/**/*',
-        env: {
-            es2020: true,
-            browser: true,
-            amd: true
-        }
-    }]
-};

frontend/.npmrc
@@ -1,0 +1,1 @@
+save=true

frontend/.stylelintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-    plugins: ['stylelint-stylistic'],
+    plugins: ['@stylistic/stylelint-plugin'],
     defaultSeverity: 'warning',
     customSyntax: 'postcss-less',
     rules: {
-        'stylistic/no-eol-whitespace': true,
-        'stylistic/block-opening-brace-space-before': 'always-multi-line',
-        'stylistic/block-opening-brace-newline-after': 'always-multi-line',
-        'stylistic/block-opening-brace-space-after': 'always-single-line',
-        'stylistic/block-closing-brace-space-before': 'always-single-line',
-        'stylistic/block-closing-brace-newline-before': 'always-multi-line',
-        'stylistic/indentation': 4
+        '@stylistic/no-eol-whitespace': true,
+        '@stylistic/block-opening-brace-space-before': 'always-multi-line',
+        '@stylistic/block-opening-brace-newline-after': 'always-multi-line',
+        '@stylistic/block-opening-brace-space-after': 'always-single-line',
+        '@stylistic/block-closing-brace-space-before': 'always-single-line',
+        '@stylistic/block-closing-brace-newline-before': 'always-multi-line',
+        '@stylistic/indentation': 4
     }
 };

frontend/eslint.config.mjs
@@ -1,0 +1,63 @@
+import globals from "globals";
+
+export default [{
+    ignores: ["js/libs", "src", "target"]
+}, {
+    rules: {
+        "no-trailing-spaces": "warn",
+        "keyword-spacing": "warn",
+
+        "space-before-function-paren": ["warn", {
+            named: "never",
+            anonymous: "always",
+            asyncArrow: "always"
+        }],
+
+        "function-call-argument-newline": ["warn", "consistent"],
+        "block-spacing": "warn",
+        "object-curly-spacing": ["warn", "always"],
+        "key-spacing": "warn",
+        "comma-dangle": "warn",
+        quotes: "warn",
+
+        "no-multiple-empty-lines": ["warn", {
+            max: 1
+        }],
+
+        semi: "warn",
+
+        indent: ["warn", 4, {
+            ignoredNodes: [
+                "Program > ExpressionStatement > CallExpression[callee.name=\"define\"] > FunctionExpression.arguments > .body"
+            ],
+
+            VariableDeclarator: 0
+        }],
+
+        "one-var": ["warn", "never"],
+        "space-infix-ops": "warn",
+        "no-multi-spaces": "warn",
+        "space-before-blocks": "warn"
+    }
+}, {
+    files: ["*.js"],
+
+    languageOptions: {
+        globals: {
+            ...globals.node
+        }
+    },
+
+    rules: {
+        quotes: ["warn", "single"]
+    }
+}, {
+    files: ["js/**/*"],
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.amd
+        }
+    }
+}];

frontend/pom.xml
@@ -46,7 +46,7 @@
               <goal>install-node-and-npm</goal>
             </goals>
             <configuration>
-              <nodeVersion>v16.13.0</nodeVersion>
+              <nodeVersion>v22.3.0</nodeVersion>
             </configuration>
           </execution>
           <execution>

frontend/style/bootstrap/variables.less
@@ -289,13 +289,13 @@
 
 // Fluid grid
 // -------------------------
-@fluidGridColumnWidth:    percentage(@gridColumnWidth/@gridRowWidth);
-@fluidGridGutterWidth:    percentage(@gridGutterWidth/@gridRowWidth);
+@fluidGridColumnWidth:    percentage((@gridColumnWidth/@gridRowWidth));
+@fluidGridGutterWidth:    percentage((@gridGutterWidth/@gridRowWidth));
 
 // 1200px min
-@fluidGridColumnWidth1200:     percentage(@gridColumnWidth1200/@gridRowWidth1200);
-@fluidGridGutterWidth1200:     percentage(@gridGutterWidth1200/@gridRowWidth1200);
+@fluidGridColumnWidth1200:     percentage((@gridColumnWidth1200/@gridRowWidth1200));
+@fluidGridGutterWidth1200:     percentage((@gridGutterWidth1200/@gridRowWidth1200));
 
 // 768px-979px
-@fluidGridColumnWidth768:      percentage(@gridColumnWidth768/@gridRowWidth768);
-@fluidGridGutterWidth768:      percentage(@gridGutterWidth768/@gridRowWidth768);
+@fluidGridColumnWidth768:      percentage((@gridColumnWidth768/@gridRowWidth768));
+@fluidGridGutterWidth768:      percentage((@gridGutterWidth768/@gridRowWidth768));

commit 094c31325777f37f3936e8a0cd8eb9670094be71

Put Less into math mode `'always'`
The upgrade to v4 of Less in b154c7b8a6767303fd9e9a443373d3c8f13faa33
introduced some changes that broke our version of Bootstrap.
Specifically, division in `.less` files now has to be in parantheses by default.
We fixed a few instances of that in the vendored files,
but missed many others.

Instead of futzing around with these files more,
this patch referts these changes, and puts Less in a compatibility mode.
**Note**: This mode is deprecated, so this is only a temporary solution,
until we find the time and money to update Bootstrap.

frontend/Gruntfile.js
@@ -17,6 +17,7 @@ module.exports = function (grunt) {
                 syncImport: true,
                 compress: true,
                 sourceMap: true,
+                math: 'always',
                 sourceMapURL: 'style/style.css.map',
                 sourceMapBasepath: 'style'
             },